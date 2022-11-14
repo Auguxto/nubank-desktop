6d1fa2b4159d702e44663d0f7cd391cd6250124a
Victor Augusto Ferreira <victoraugusferreira@gmail.com>
2022-11-13 21:15:35 -0300

    add: header componente, user infos and buttons

--- src/renderer/screens/Home/index.tsx
+++ src/renderer/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import { Eye, Settings } from "lucide-react";
 import React from "react";
 
 import * as S from "./styles";
@@ -5,7 +6,34 @@ import * as S from "./styles";
 const HomeScreen: React.FC = () => {
   return (
     <S.Container>
-      <S.Avatar />
+      <S.Header>
+        <S.Avatar />
+        <S.Infos>
+          <S.Name>Victor Augusto Ferreira</S.Name>
+          <S.Fields>
+            <S.Field>
+              <S.Title>Agência:</S.Title>
+              <S.Value>0000</S.Value>
+            </S.Field>
+            <S.Field>
+              <S.Title>Conta:</S.Title>
+              <S.Value>00000000-0</S.Value>
+            </S.Field>
+          </S.Fields>
+          <S.Field>
+            <S.Title>CPF:</S.Title>
+            <S.Value>000.000.000-00</S.Value>
+          </S.Field>
+        </S.Infos>
+        <S.Buttons>
+          <S.Button>
+            <Settings size={30} color="#FFFFFF" />
+          </S.Button>
+          <S.Button>
+            <Eye size={30} color="#FFFFFF" />
+          </S.Button>
+        </S.Buttons>
+      </S.Header>
     </S.Container>
   );
 };
--- src/renderer/screens/Home/styles.ts
+++ src/renderer/screens/Home/styles.ts
@@ -4,7 +4,19 @@ import avatarImg from "../../../assets/avatar.png";
 export const Container = styled.div`
   display: flex;
 
+  flex-direction: column;
+
+  background-color: #ffffff;
+`;
+
+export const Header = styled.div`
+  display: flex;
+
   padding: 25px;
+
+  background-color: #8109d1;
+
+  user-select: none;
 `;
 
 export const Avatar = styled.img.attrs({
@@ -16,4 +28,77 @@ export const Avatar = styled.img.attrs({
   border-radius: 150px;
 
   border: 3px solid #ffffff;
+`;
+
+export const Infos = styled.div`
+  display: flex;
+
+  flex: 1;
+
+  flex-direction: column;
+
+  gap: 5;
+
+  margin-left: 20px;
+`;
+
+export const Name = styled.span`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 26px;
+
+  color: #ffffff;
+`;
+
+export const Fields = styled.div`
+  display: flex;
+
+  gap: 10px;
+`;
+
+export const Field = styled.div`
+  display: flex;
+
+  gap: 5px;
+
+  margin: 5px 0px;
+`;
+
+export const Title = styled.span`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+
+  color: #ffffff;
+`;
+
+export const Value = styled.span`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+
+  color: #ffffff;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+
+  height: 35px;
+
+  gap: 20px;
+`;
+
+export const Button = styled.button`
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+
+  background-color: transparent;
+  border: none;
+
+  cursor: pointer;
 `;
